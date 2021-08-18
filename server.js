c90b143a83735f4bd4d60c82c3a292553994f47a
elia-dot <eliacohen100@gmail.com>
2021-08-18 21:44:57 +0300

    prepared for deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
 
-if (proccess.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
